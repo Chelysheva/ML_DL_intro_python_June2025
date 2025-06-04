184f802c64ab694e65668da7a04ba1f715ce3210
Irina Chelysheva <chelysheva.ii@gmail.com>
2025-06-04 12:58:44 +0100

    Update ML_Python_day1_regression.py

--- ML_Python_day1_regression.py
+++ ML_Python_day1_regression.py
@@ -5,7 +5,7 @@ Machine Learning in Python with scikit-learn
 COVID dataset analysis (regression problem)
 Author: Irina Chelysheva
 
-Date: April 28th, 2025
+Date: June 5th, 2025
 COVID dataset extracted from OurWorldInData (https://github.com/owid/covid-19-data)
 """
 
@@ -24,7 +24,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 # 2. Set working directory, load, explore and prepare the dataset
-os.chdir('ML_DL_intro_python_HT2025_3-main')
+os.chdir('ML_DL_intro_python_June2025-main')
 df = pd.read_csv('owid_covid_for_ML_short.csv')
 print("Total data points",df.shape[0])
 print("Total number of features(as number of columns) are ", df.shape[1])
